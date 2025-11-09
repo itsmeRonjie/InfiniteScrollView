2eb8a617d4e23c9429d3e964481ad55206da2878
itsmeRonjie <itsme.Ronjie@gmail.com>
2025-11-09 18:52:49 +0800

    Initial Commit

---
+++ InfiniteScrollView/InfiniteScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollViewApp.swift
+//  InfiniteScrollView
+//
+//  Created by Ronjie Man-on on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
